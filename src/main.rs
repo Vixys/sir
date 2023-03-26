fdfc06aa8a2a55ff7d0ab45b6a59fbf1cb764fbd
Sébastien Marichal <sebmarichal@gmail.com>
2023-03-26 12:55:59 +0000

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,58 @@
 use clap::Parser;
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read};
+use std::path::PathBuf;
 
 const BUFFER_SIZE: usize = 512;
 
 /// Concatenate FILE(s) to standard output.
 /// 
-/// rcat is a Rust implementation of cat
+/// rcat is a Rust implementation of cat(1)
 #[derive(Parser)]
-struct RcatArgs {
+struct RCatArgs {
     /// File(s) to concatenante to standard output.
     file: Vec<std::path::PathBuf>
 }
 
-fn print_file(file: &mut File) {
+#[derive(Debug)]
+struct RCatError(String);
+
+impl std::fmt::Display for RCatError {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fmt.write_str(self.0.as_str())
+    }
+}
+
+fn print_file(file: &mut File) -> Result<(), RCatError> {
     let mut buffer = [0; BUFFER_SIZE];
 
     loop {
-        let read = match file.read(&mut buffer) {
-            Ok(nb) => nb,
-            Err(err) => panic!("Unknown error while reading file: {}", err),
-        };
-        
+        let read = file.read(&mut buffer).map_err(|err| RCatError(err.to_string()))?;
 
-        match String::from_utf8(buffer[..read].to_vec()){
-            Ok(content) => print!("{}", content),
-            Err(err) => panic!("Unknown error converting to UTF8: {}", err),
-        }
+        print!("{}", String::from_utf8(buffer[..read].to_vec()).map_err(|err| RCatError(err.to_string()))?);
 
         if read < BUFFER_SIZE {
             break;
         }
     }
+    Ok(())
 }
 
+fn open_file(path: &PathBuf) -> Result<File, RCatError> {
+    Ok(File::open(&path).map_err(|err| RCatError(err.to_string()))?)
+}
+
+
+fn execute(path: &PathBuf) -> Result<(), RCatError> {
+    print_file(&mut open_file(&path)?)?;
+    Ok(())
+}
 fn main() {
-    let args = RcatArgs::parse();
+    let args = RCatArgs::parse();
 
     for path in args.file {
-        match File::open(&path) {
-            Ok(mut file) => print_file(&mut file),
-            Err(_) => eprintln!("rcat: {}: No such file or directory", path.display()),
+        if let Err(err) = execute(&path) {
+            eprintln!("rcat: {}: {}", path.display(), err);
         }
     }
 }
